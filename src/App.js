f5a975ef7d00535de83b058aaf9de007cd745e13
SathyanarayananPro <sathyanarayanan01072000@gmail.com>
2025-01-29 15:36:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -253,7 +253,7 @@ function SelectedMovie({
       async function movieDetail() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
