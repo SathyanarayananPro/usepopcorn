193dd1918541a122be7682b65dcb56e3b54d51ac
SathyanarayananPro <sathyanarayanan01072000@gmail.com>
2025-01-29 15:27:31 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StartRating";
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="Blue" maxRating={10} setMovieRating={setMovieRating} />
-      <p>This Movie was rated as {movieRating}</p>
-    </div>
-  );
-}
+// import StarRating from "./StartRating";
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="Blue" maxRating={10} setMovieRating={setMovieRating} />
+//       <p>This Movie was rated as {movieRating}</p>
+//     </div>
+//   );
+// }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
